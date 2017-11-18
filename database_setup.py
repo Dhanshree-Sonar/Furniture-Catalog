ae702ad3973a065c1d5dac3137a0c710dbb54182
Dhanshree-Sonar <31264948+Dhanshree-Sonar@users.noreply.github.com>
2017-11-17 23:42:46 -0500

    Update database_setup.py
    python file is styled according to pep8 standards

--- database_setup.py
+++ database_setup.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
 
@@ -19,37 +20,37 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id = Column(Integer,ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
     def serialize(self):
         # Returns object data in easily serializable format
         return{
-            'id' : self.id,
-            'name' : self.name,
+            'id': self.id,
+            'name': self.name,
         }
 
 
 class Item(Base):
     __tablename__ = 'item'
 
-    id = Column(Integer, primary_key = True)
-    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
     description = Column(String(250))
-    cat_id = Column(Integer,ForeignKey('category.id'))
+    cat_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-    user_id = Column(Integer,ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
     @property
     def serialize(self):
         # Returns object data in easily serializable format
         return{
-            'id' : self.id,
-            'name' : self.name,
-            'description' : self.description,
-            'cat_id' : self.cat_id,
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'cat_id': self.cat_id,
         }
 
 
